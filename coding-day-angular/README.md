0cf935e3759e91ded6c0ec478468fa2777eedfde
altbdoor <lancersupraskyline@gmail.com>
2020-08-08 14:53:26 +0800

    add angular v10 project with ng-tailwindcss

---
+++ coding-day-angular/README.md
@@ -0,0 +1,9 @@
+# Playground
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.5.
+
+```sh
+ng new playground --routing=true --skipGit=true --skipInstall=true --strict=true --style=scss
+```
+
+[tailwindcss](https://tailwindcss.com/) is setup into the project with [ng-tailwindcss](https://github.com/tehpsalmist/ng-tailwindcss) with all the default values.
---
+++ coding-day-angular/ng-tailwind.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // Tailwind Paths
+    configJS: 'tailwind\\tailwind.config.js',
+    sourceCSS: 'tailwind\\source.scss',
+    outputCSS: 'src\\tailwind.css',
+    watchRelatedFiles: [],
+    // Sass
+    sass: false,
+    // PurgeCSS Settings
+    purge: false,
+    keyframes: false,
+    fontFace: false,
+    rejected: false,
+    whitelist: [],
+    whitelistPatterns: [],
+    whitelistPatternsChildren: [],
+    extensions: ['.ts', '.html', '.js'],
+    extractors: [],
+    content: [],
+};
